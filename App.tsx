c2422deb223cc931bddada8faacd8c2c84bc4596
naftalimurgor <murgornaftali@gmail.com>
2023-12-02 15:06:59 +0300

    Feat:[wip] add walk through screens #1

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeSplash from './src/app/screens/WelcomeSplash';
+import WelcomeWalkThrough from './src/app/screens/WelcomeWalkThrough';
 
 export default function App() {
   return (
-    <WelcomeSplash/>
+    // <WelcomeSplash/>
+    <WelcomeWalkThrough/>
   );
 }
---
+++ src/app/screens/WelcomeWalkThrough.tsx
@@ -0,0 +1,62 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { COLORS } from '../COLORS'
+import SplashLogo from '../../components/SplashLogo'
+import { useFonts } from 'expo-font'
+import { Poppins_500Medium, Poppins_700Bold } from '@expo-google-fonts/poppins'
+
+export default function WelcomeWalkThrough() {
+    // Loads non-system font asynchronously, set as fontFamily: 'Poppins_500Medium'
+    const [fontsLoaded] = useFonts({
+        Poppins_500Medium, 
+        Poppins_700Bold
+    })
+
+    return (
+        <View style={styles.welcomeContainer}>
+            <View style={styles.viewLogoContainer}>
+                <SplashLogo />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.ctaHeadingText}>
+                    Welcome to Our Bitgesell Mobile Wallet
+                </Text>
+                <Text style={styles.welcomeText}>
+                    Welcome to Bitgesell Wallet
+                    –Start managing your digital assets with ease and confidence.
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    welcomeContainer: {
+        backgroundColor: COLORS.ACCENT,
+        flex: 1
+    },
+    viewLogoContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '40%'
+    },
+    textContainer: {
+        flex: 0.6,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: '50%'
+    },
+    ctaHeadingText: {
+        fontSize: 24,
+        marginBottom: 20,
+        fontFamily: 'Poppins_600SemiBold',
+        color: COLORS.WHITE
+    },
+    welcomeText: {
+        color: COLORS.WHITE,
+        fontSize: 14,
+        fontFamily: 'Poppins_500Medium'
+    }
+})
