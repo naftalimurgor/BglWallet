3c8058a88c71e8ba7a47bc571737dda2690c9614
naftalimurgor <murgornaftali@gmail.com>
2023-12-09 23:55:21 +0300

    fix non-system font warning using useFont hook

--- app/WelcomeSplash.tsx
+++ app/WelcomeSplash.tsx
@@ -1,23 +1,26 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { LayoutChangeEvent, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { COLORS } from 'app/COLORS'
 import SplashLogo from '@/components/SplashLogo'
 import { useFonts } from 'expo-font'
 import { Poppins_500Medium } from '@expo-google-fonts/poppins'
 
-export default function WelcomeSplash() {
+interface Props {
+    onLayout:((event: LayoutChangeEvent) => void) | undefined
+}
+export default function WelcomeSplash({onLayout}: Props) {
     // Loads non-system font asynchronously, set as fontFamily: 'Poppins_500Medium'
     const [fontsLoaded] = useFonts({
         Poppins_500Medium
     })
 
     return (
-        <View style={styles.welcomeContainer}>
+        <View style={styles.welcomeContainer} onLayout={onLayout}>
             <View style={styles.viewLogoContainer}>
                 <SplashLogo />
             </View>
-            <View style={styles.textContainer}>
-                <Text style={styles.welcomeText}>
+            <View style={[styles.textContainer,]}>
+                <Text style={[styles.welcomeText, {fontFamily: fontsLoaded? 'Poppins_500Medium' : ''}]}>
                     Welcome to our new crypto wallet mobile app
                 </Text>
             </View>
