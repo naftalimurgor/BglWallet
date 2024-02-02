03a49ed29e029caa3f1053f5f0503f6c2a466802
naftalimurgor <murgornaftali@gmail.com>
2024-02-02 13:49:40 +0300

    feat: createWallet redux integration

--- app/(app)/create-new-wallet.tsx
+++ app/(app)/create-new-wallet.tsx
@@ -6,23 +6,36 @@ import { Poppins_600SemiBold, Poppins_700Bold } from '@expo-google-fonts/poppins
 import { Feather } from '@expo/vector-icons'
 import { StatusBar } from 'expo-status-bar'
 import { router } from 'expo-router'
-import { emailRegex } from '@/api/User'
+import { emailRegex } from '@/features/api/User'
+import { useStorageState } from '../hooks/useStorageState'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 const CreateNewWallet = () => {
+
+  // 1. dispatch(action(payload))
+  const dispatch = useDispatch()
+  // 2. sample state access
+  // @ts-ignore 
+  const wallet = useSelector(state=> state.wallet)
+
   const [fontsLoaded] = useFonts({
     Poppins_600SemiBold,
     Poppins_700Bold
   })
 
+  const [session, setSession] = useStorageState('session')
+
   const [password, setPassword] = useState<string>('')
   const [email, setEmail] = useState<string>('')
 
   const handleSubmit = async () => {
-
+    // use a thunk to perform create wallet with the appropriate error states
+    router.replace('/(home)/')
   }
 
   const [secure, setSecure] = useState<boolean>(true)
-  
+
   return (
     <View style={styles.createWalletContainer}>
       <StatusBar backgroundColor='#824FF4' />
@@ -77,11 +90,9 @@ const CreateNewWallet = () => {
           🔒 Keep this password safe as it will be used to keep your wallet safe on this device.
         </Text>
         <View style={styles.submitButtonContainer}>
-          <Pressable style={[styles.submitButton, {opacity: password !=='' && emailRegex(email) ? 1: 0.5}]} 
-          disabled={password === '' && !emailRegex(email)}
-          onPress={() => {
-            router.push('/(home)/')
-          }}>
+          <Pressable style={[styles.submitButton, { opacity: password !== '' && emailRegex(email) ? 1 : 0.5 }]}
+            disabled={password === '' && !emailRegex(email)}
+            onPress={handleSubmit}>
             <Text style={styles.submitButtonText}>Create A New Wallet</Text>
           </Pressable>
         </View>
@@ -96,7 +107,8 @@ const styles = StyleSheet.create({
   createWalletContainer: {
     flex: 1,
     backgroundColor: COLORS.WHITE,
-    padding: 20
+    padding: 20,
+    paddingLeft: 20,
   },
   walletTextContainer: {
     flexDirection: 'column',
@@ -135,14 +147,14 @@ const styles = StyleSheet.create({
     fontWeight: '700',
   },
   passwordInput: {
-    width: 335,
+    width: 300,
     fontSize: 16,
     height: 81,
     marginLeft: 12,
     // marginRight: 28,
     borderBottomColor: COLORS.WHITE004,
     borderStyle: 'solid',
-    borderWidth: 2,
+    borderWidth: 1,
     borderTopColor: COLORS.WHITE,
     borderEndColor: COLORS.WHITE,
     borderLeftColor: COLORS.WHITE,
